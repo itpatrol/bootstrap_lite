75fce3d50eaf6031e6275d8e0e81f409d37bf984
Gor Martsen <Gor.martsen@gmail.com>
2015-10-31 23:28:06 -0400

    Debug code

--- template.php
+++ template.php
@@ -224,7 +224,8 @@ function bootstrap_lite_fieldset($variables) {
  * @ingroup themeable
  */
 function bootstrap_lite_button($variables) {
-  print_r($variables['element']);
+  print_r($variables);
+
   if(isset($variables['element']['#attributes']['class'])){
     $default = TRUE;
     foreach($variables['element']['#attributes']['class'] as $key => $class){
