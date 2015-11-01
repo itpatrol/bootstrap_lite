10d14d1854ba7667d5f49a44c82684ee0e82b34a
Gor Martsen <Gor.martsen@gmail.com>
2015-10-31 22:34:56 -0400

    #1 Support fluid container for content as well

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -108,7 +108,7 @@
   </div>
 </header>
 
-<div class="main-container container">
+<div class="main-container <?php print $container_class;?>">
 
   <header role="banner" id="page-header">
     <?php if (!empty($site_slogan)): ?>
