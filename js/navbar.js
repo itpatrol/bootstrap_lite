bdc7eb52d2c49c03992f9b4583e75894fbd12a52
Gor Martsen <Gor.martsen@gmail.com>
2015-11-04 21:00:18 -0500

    Only change when nabber is fixed top

--- js/navbar.js
+++ js/navbar.js
@@ -8,23 +8,25 @@
   'use strict';
 
   function BootstrapNavbarOffsetChangeHandler(e, offsets) {
-    var NavbarMarginBottom = 20;
-    var toolbarMargin = $('#toolbar-bar').height();
-    
-    if($('body').hasClass('toolbar-fixed')){
+    if( $('#navbar').hasClass('navbar-fixed-top')){
+      var NavbarMarginBottom = 20;
+      var toolbarMargin = $('#toolbar-bar').height();
       
-      if($('#toolbar-item-administration-tray').hasClass('is-active') && $('#toolbar-item-administration-tray').hasClass('toolbar-tray-horizontal')){
-        toolbarMargin = toolbarMargin + $('#toolbar-item-administration-tray').height();
+      if($('body').hasClass('toolbar-fixed')){
+        
+        if($('#toolbar-item-administration-tray').hasClass('is-active') && $('#toolbar-item-administration-tray').hasClass('toolbar-tray-horizontal')){
+          toolbarMargin = toolbarMargin + $('#toolbar-item-administration-tray').height();
+        }
+        
+        $('#navbar').css('margin-top', toolbarMargin + 'px');
+        $('#toolbar-bar').css('padding-top', '0px');
+      } else {
+        $('#toolbar-bar').css('padding-top', $('#navbar').height() + 'px');
       }
-      
-      $('#navbar').css('margin-top', toolbarMargin + 'px');
-      $('#toolbar-bar').css('padding-top', '0px');
-    } else {
-      $('#toolbar-bar').css('padding-top', $('#navbar').height() + 'px');
+      var dataOffsetTop = toolbarMargin +  $('#navbar').height();
+      $('#navbar').attr('data-offset-top', dataOffsetTop);
+      $('body').css('margin-top', NavbarMarginBottom + 'px');
     }
-    var dataOffsetTop = toolbarMargin +  $('#navbar').height();
-    $('#navbar').attr('data-offset-top', dataOffsetTop);
-    $('body').css('margin-top', NavbarMarginBottom + 'px');
   }
   
   // Bind to custom Drupal events.
