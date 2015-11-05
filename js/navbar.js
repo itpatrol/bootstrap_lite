de6ba76e790bf6d5c6265adee4f4ab1c35ea9cec
Gor Martsen <Gor.martsen@gmail.com>
2015-11-04 21:16:36 -0500

    Clean code

--- js/navbar.js
+++ js/navbar.js
@@ -6,30 +6,6 @@
 (function ($, Drupal, displace) {
 
   'use strict';
-
-  function BootstrapNavbarOffsetChangeHandler(e, offsets) {
-
-    if( $('#navbar').hasClass('navbar-fixed-top')){
-      var NavbarMarginBottom = 20;
-      var toolbarMargin = $('#toolbar-bar').height();
-      
-      if($('body').hasClass('toolbar-fixed')){
-        
-        if($('#toolbar-item-administration-tray').hasClass('is-active') && $('#toolbar-item-administration-tray').hasClass('toolbar-tray-horizontal')){
-          toolbarMargin = toolbarMargin + $('#toolbar-item-administration-tray').height();
-        }
-        
-        $('#navbar').css('margin-top', toolbarMargin + 'px');
-        $('#toolbar-bar').css('padding-top', '0px');
-      } else {
-        $('#toolbar-bar').css('padding-top', $('#navbar').height() + 'px');
-      }
-      var dataOffsetTop = toolbarMargin +  $('#navbar').height();
-      $('#navbar').attr('data-offset-top', dataOffsetTop);
-      $('body').css('margin-top', NavbarMarginBottom + 'px');
-    }
-  }
-  
   if( $('#navbar').hasClass('navbar-fixed-top')){
     alert(1);
     var NavbarMarginBottom = 20;
@@ -51,15 +27,4 @@
     $('body').css('margin-top', NavbarMarginBottom + 'px');
     $('body').css('padding-top', dataOffsetTop + 'px');
   }
-    
-  // Bind to custom Drupal events.
-  $(document).on({
-    /**
-     * Recalculate BootstrapNavbar.topOffset when viewport is resized.
-     *
-     * @ignore
-     */
-   // 'drupalViewportOffsetChange.BootstrapNavbar': BootstrapNavbarOffsetChangeHandler
-  });
-
 }(jQuery, Drupal, window.parent.Drupal.displace));
