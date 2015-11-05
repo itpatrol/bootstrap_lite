3e1c248d6e7cab0aa0eb7b33d7d7d58f4e0723d1
Gor Martsen <Gor.martsen@gmail.com>
2015-11-04 19:56:11 -0500

    Fix typo

--- js/navbar.js
+++ js/navbar.js
@@ -8,7 +8,7 @@
   'use strict';
 
   function BootstrapNavbarOffsetChangeHandler(e, offsets) {
-    var toolbarMargin = ('#toolbar-bar').height() + ('#toolbar-item-administration-tray').height();
+    var toolbarMargin = $('#toolbar-bar').height() + $('#toolbar-item-administration-tray').height();
     alert(toolbarMargin);
     ('#navbar').attr('data-offset-top', toolbarMargin);
   }
