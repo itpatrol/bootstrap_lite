3f65496adc90645cb14256927684c56db17641e0
Gor Martsen <Gor.martsen@gmail.com>
2015-11-04 21:14:11 -0500

    Debug code

--- js/navbar.js
+++ js/navbar.js
@@ -8,7 +8,7 @@
   'use strict';
 
   function BootstrapNavbarOffsetChangeHandler(e, offsets) {
-    alert(1);
+
     if( $('#navbar').hasClass('navbar-fixed-top')){
       var NavbarMarginBottom = 20;
       var toolbarMargin = $('#toolbar-bar').height();
@@ -30,6 +30,27 @@
     }
   }
   
+  if( $('#navbar').hasClass('navbar-fixed-top')){
+    alert(1);
+    var NavbarMarginBottom = 20;
+    var toolbarMargin = $('#toolbar-bar').height();
+    
+    if($('body').hasClass('toolbar-fixed')){
+      
+      if($('#toolbar-item-administration-tray').hasClass('is-active') && $('#toolbar-item-administration-tray').hasClass('toolbar-tray-horizontal')){
+        toolbarMargin = toolbarMargin + $('#toolbar-item-administration-tray').height();
+      }
+      
+      $('#navbar').css('margin-top', toolbarMargin + 'px');
+      $('#toolbar-bar').css('padding-top', '0px');
+    } else {
+      $('#toolbar-bar').css('padding-top', $('#navbar').height() + 'px');
+    }
+    var dataOffsetTop = toolbarMargin +  $('#navbar').height();
+    $('#navbar').attr('data-offset-top', dataOffsetTop);
+    $('body').css('margin-top', NavbarMarginBottom + 'px');
+  }
+    
   // Bind to custom Drupal events.
   $(document).on({
     /**
